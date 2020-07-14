f2a3b5feaf2f7d4138734b1dd26c3ecd9f27b9d0
friendly-skill-4649 <jeffrey.palmowski@hotmail.com>
2020-07-14 18:08:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,11 +2,12 @@
 function happyHolidays() {
   return "Happy holidays!"
 }
-
 funtion happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
-
 funtion happyCustomHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
+}
+funtion holidayCountdown(holiday, days) {
+  return `It/s ${days} days until ${holiday}!`
 }
